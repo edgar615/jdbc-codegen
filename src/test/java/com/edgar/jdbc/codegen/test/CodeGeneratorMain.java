21d58dbfeb3d54c2908c17d8a173743545108bbc
Edgar Zhang <edgar615@gmail.com>
2015-09-08 20:36:09 +0800

    调整wordutils

---
+++ src/test/java/com/edgar/jdbc/codegen/test/CodeGeneratorMain.java
@@ -0,0 +1,15 @@
+package com.edgar.jdbc.codegen.test;
+
+import com.edgar.jdbc.codegen.CodeGenerator;
+
+/**
+ * Created by Administrator on 2015/6/9.
+ */
+public class CodeGeneratorMain {
+
+    public static void main(String[] args) throws Exception {
+        CodeGenerator generator = new CodeGenerator();
+        generator.setPropertiesFile("src/test/resources/codegenerator.properties");
+        generator.generate();
+    }
+}
