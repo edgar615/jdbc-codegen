83fae0a6c2e5e659f5c78767027ad2a3e43123c8
Edgar <edgar615@gmail.com>
2018-01-25 22:30:12 +0800

    修改格式

--- src/test/java/com/github/edgar615/jdbc/codegen/test/FetchDataFromTest.java
+++ src/test/java/com/github/edgar615/jdbc/codegen/test/FetchDataFromTest.java
@@ -11,14 +11,19 @@ public class FetchDataFromTest {
   public static void main(String[] args) throws Exception {
     CodegenOptions options = new CodegenOptions().setUsername("root")
             .setPassword("123456")
-            .addGenTable("test")
-            .addGenTable("user")
+//            .addGenTable("dict")
+//            .addGenTable("dict_item")
+            .addGenTable("sys_resource")
+            .addGenTable("sys_subsystem")
+            .addGenTable("sys_permission")
             .setIgnoreColumnsStr("created*,updated_on")
             .setGenRule(true)
             .setJdbcUrl(
-                    "jdbc:mysql://localhost:3306/user")
+                    "jdbc:mysql://localhost:3306/sys")
 //            .setTableNamePattern("device")
-            .setIgnoreColumnsStr("photo_path,degree");
+//                .setIgnoreColumnsStr("photo_path,degree")
+            .setSrcFolderPath("src/main/java")
+            .setDomainPackage("com.github.edgar615.om.system.basic.domain");
 
     new Generator(options).generate();
 
