3e0688037242e5801cd8417b3b833b7e77f518cb
edgar615 <edgar615@gmail.com>
2018-05-17 17:46:44 +0800

    增加从fromMap的方法

--- src/main/java/com/github/edgar615/jdbc/codegen/gen/HelperSource.java
+++ src/main/java/com/github/edgar615/jdbc/codegen/gen/HelperSource.java
@@ -1,9 +1,11 @@
 package com.github.edgar615.jdbc.codegen.gen;
 
+import com.github.edgar615.jdbc.codegen.db.Column;
 import com.github.jknack.handlebars.Options;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 public class HelperSource {
 
@@ -19,6 +21,21 @@ public class HelperSource {
       return options.fn(this);
     }
     return options.inverse(this);
+  }
+
+  public CharSequence parameterType(Object v1, Object v2, Options options) throws IOException {
+    if (v1 == null) {
+      return options.inverse(this);
+    }
+    if (v2 == null) {
+      return options.inverse(this);
+    }
+    List<Column> columns = (List<Column>) v2;
+    Optional<String> optional = columns.stream()
+            .filter(c -> v1.toString().equals(c.getLowerCamelName()))
+            .map(c ->  c.getParameterType().getName())
+            .findFirst();
+    return optional.orElse("");
   }
 
 }
