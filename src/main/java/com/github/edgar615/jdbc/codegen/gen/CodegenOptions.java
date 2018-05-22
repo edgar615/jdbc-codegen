80f4760c91c7b19e0b7c7abd18ef9c47902f2814
edgar615 <edgar615@gmail.com>
2018-05-22 10:09:35 +0800

    将公共部分移动到mysql-mapping项目

--- src/main/java/com/github/edgar615/jdbc/codegen/gen/CodegenOptions.java
+++ src/main/java/com/github/edgar615/jdbc/codegen/gen/CodegenOptions.java
@@ -1,87 +1,25 @@
 package com.github.edgar615.jdbc.codegen.gen;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Strings;
+import com.github.edgar615.mysql.mapping.TableMappingOptions;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 /**
  * Created by Edgar on 2016/4/1.
  *
  * @author Edgar  Date 2016/4/1
  */
-public class CodegenOptions {
+public class CodegenOptions extends TableMappingOptions {
   public static final String DEFAULT_SRC_FOLDER_PATH = "src";
 
   //domain
   public static final String DEFAULT_DOMAIN_PACKAGE = "com.github.edgar615.code.domain";
 
-  //支持通配符,product, exa*, *e
-  public static final String DEFAULT_IGNORE_TABLES = null;
-
-  //支持通配符,product, exa*, *e
-  public static final String DEFAULT_IGNORE_COLUMN = null;
-
-  public static final String DEFAULT_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-
-  public static final String DEFAULT_JDBC_ARG = "";
-
-  public static final String DEFAULT_HOST = "localhost";
-
-  public static final int DEFAULT_PORT = 3306;
-
-  public static final String DEFAULT_DATABASE = "test";
-
-  public static final String DEFAULT_USERNAME = "root";
-
-  public static final String DEFAULT_PASSWORD = "";
-
   private static final boolean DEFAULT_GEN_RULE = false;
-
-  //忽略的字段
-  private final List<String> ignoreColumnList = new ArrayList<String>();
-
-  //使用前缀匹配忽略的表
-  private final List<String> ignoreColumnStartsWithPattern = new ArrayList<String>();
-
-  //使用后缀匹配忽略的表
-  private final List<String> ignoreColumnEndsWithPattern = new ArrayList<String>();
-
-  //忽略的表
-  private final List<String> ignoreTableList = new ArrayList<String>();
-
-  //使用前缀匹配忽略的表
-  private final List<String> ignoreTableStartsWithPattern = new ArrayList<String>();
-
-  //使用后缀匹配忽略的表
-  private final List<String> ignoreTableEndsWithPattern = new ArrayList<String>();
-
-  //只生成这些表
-  private final List<String> tableList = new ArrayList<>();
 
   private String srcFolderPath = DEFAULT_SRC_FOLDER_PATH;
 
   private String domainPackage = DEFAULT_DOMAIN_PACKAGE;
-
-  private String ignoreTablesStr = DEFAULT_IGNORE_TABLES;
-
-  private String ignoreColumnsStr = DEFAULT_IGNORE_COLUMN;
-
-  private String driverClass = DEFAULT_JDBC_DRIVER;
-
-  private String host = DEFAULT_HOST;
-
-  private int port = DEFAULT_PORT;
-
-  private String database = DEFAULT_DATABASE;
-
-  private String jdbcArg = DEFAULT_JDBC_ARG;
-
-  private String username = DEFAULT_USERNAME;
-
-  private String password = DEFAULT_PASSWORD;
 
   private boolean genRule = DEFAULT_GEN_RULE;
 
@@ -112,65 +50,26 @@ public class CodegenOptions {
     return this;
   }
 
-  public String getJdbcUrl() {
-    String jdbcUrl =  "jdbc:mysql://"+ host + ":" + port + "/" + database;
-    if (!Strings.isNullOrEmpty(jdbcArg)) {
-      jdbcUrl += "?" + jdbcArg;
-    }
-    return jdbcUrl;
-  }
-
-  public String getHost() {
-    return host;
-  }
-
   public CodegenOptions setHost(String host) {
-    this.host = host;
+    super.setHost(host);
     return this;
-  }
-
-  public int getPort() {
-    return port;
   }
 
   public CodegenOptions setPort(int port) {
-    this.port = port;
+    super.setPort(port);
     return this;
-  }
-
-  public String getDatabase() {
-    return database;
   }
 
   public CodegenOptions setDatabase(String database) {
-    this.database = database;
+    super.setDatabase(database);
     return this;
-  }
-
-  public String getJdbcArg() {
-    return jdbcArg;
   }
 
   public CodegenOptions setJdbcArg(String jdbcArg) {
-    this.jdbcArg = jdbcArg;
+    super.setJdbcArg(jdbcArg);
     return this;
   }
 
-  public List<String> getIgnoreColumnList() {
-    return ignoreColumnList;
-  }
-
-  public List<String> getIgnoreTableList() {
-    return ignoreTableList;
-  }
-
-  public List<String> getIgnoreTableStartsWithPattern() {
-    return ignoreTableStartsWithPattern;
-  }
-
-  public List<String> getIgnoreTableEndsWithPattern() {
-    return ignoreTableEndsWithPattern;
-  }
 
   public String getDomainPackage() {
     return domainPackage;
@@ -181,105 +80,40 @@ public class CodegenOptions {
     return this;
   }
 
-  public String getIgnoreTablesStr() {
-    return ignoreTablesStr;
-  }
-
   public CodegenOptions setIgnoreTablesStr(String ignoreTablesStr) {
-    this.ignoreTablesStr = ignoreTablesStr;
-    this.setIgnoreTable();
+    super.setIgnoreTablesStr(ignoreTablesStr);
     return this;
   }
 
   public CodegenOptions addGenTable(String tableName) {
-    this.tableList.add(tableName);
+    super.addGenTable(tableName);
     return this;
-  }
-
-  public List<String> getTableList() {
-    return tableList;
   }
 
   public CodegenOptions addGenTables(List<String> tableNames) {
-    this.tableList.addAll(tableNames);
+    super.addGenTables(tableNames);
     return this;
-  }
-
-  public String getIgnoreColumnsStr() {
-    return ignoreColumnsStr;
   }
 
   public CodegenOptions setIgnoreColumnsStr(String ignoreColumnsStr) {
-    this.ignoreColumnsStr = ignoreColumnsStr;
-    this.setIgnoreColumn();
+    super.setIgnoreColumnsStr(ignoreColumnsStr);
     return this;
   }
 
-  public List<String> getIgnoreColumnStartsWithPattern() {
-    return ignoreColumnStartsWithPattern;
-  }
-
-  public List<String> getIgnoreColumnEndsWithPattern() {
-    return ignoreColumnEndsWithPattern;
-  }
-
-  public String getDriverClass() {
-    return driverClass;
-  }
 
   public CodegenOptions setDriverClass(String driverClass) {
-    this.driverClass = driverClass;
+    super.setDriverClass(driverClass);
     return this;
-  }
-
-  public String getUsername() {
-    return username;
   }
 
   public CodegenOptions setUsername(String username) {
-    this.username = username;
+    super.setUsername(username);
     return this;
-  }
-
-  public String getPassword() {
-    return password;
   }
 
   public CodegenOptions setPassword(String password) {
-    this.password = password;
+    super.setPassword(password);
     return this;
   }
 
-  private void setIgnoreTable() {
-    if (!Strings.isNullOrEmpty(ignoreTablesStr)) {
-      StringTokenizer strTok = new StringTokenizer(ignoreTablesStr, ",");
-      while (strTok.hasMoreTokens()) {
-        String token = strTok.nextToken().toLowerCase().trim();
-        if (CharMatcher.anyOf("*").indexIn(token) == 0) {
-          this.ignoreTableEndsWithPattern.add(token.substring(1, token.length()));
-        } else if (CharMatcher.anyOf("*").lastIndexIn(token) == token.length() - 1) {
-          this.ignoreTableStartsWithPattern.add(token.substring(0, token.length() - 1));
-        } else {
-          this.ignoreTableList.add(token);
-        }
-      }
-    }
-  }
-
-  private void setIgnoreColumn() {
-
-    if (!Strings.isNullOrEmpty(ignoreColumnsStr)) {
-      StringTokenizer strTok = new StringTokenizer(ignoreColumnsStr, ",");
-      while (strTok.hasMoreTokens()) {
-        String token = strTok.nextToken().toLowerCase().trim();
-        if (CharMatcher.anyOf("*").indexIn(token) == 0) {
-          this.ignoreColumnEndsWithPattern.add(token.substring(1, token.length()));
-        } else if (CharMatcher.anyOf("*").lastIndexIn(token) == token.length() - 1) {
-          this.ignoreColumnStartsWithPattern.add(token.substring(0, token.length() - 1));
-        } else {
-          this.ignoreColumnList.add(token);
-        }
-      }
-    }
-  }
 }
